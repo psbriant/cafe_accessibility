b984a7ff48be9d3308a87ef8415c791c84d754de
psbriant <briantp@email.arizona.edu>
2026-07-31 17:43:54 -0700

    Initial commit that successfully cleans gis dataset

---
+++ get_bus_stops.py
@@ -0,0 +1,37 @@
+"""
+Description: 
+Pull geographic metadata for King County Metro bus stops for Seattle from the 
+King County GIS Center.
+"""
+
+import os
+
+import numpy as np
+import pandas as pd
+
+# global path variables
+main_path = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
+
+# Read in bus stop location data from King County Metro
+bus_stops = pd.read_csv(f'{main_path}king_county_bus_stop_data.csv')
+
+bus_stops = bus_stops[['STOP_ID', 'STOP_STATUS', 'XCOORD', 'YCOORD', 'STOP_TYPE', 'ON_STREET_NAME', 'HASTUS_CROSS_STREET_NAME', 'AUTH_NAME', 'ZIPCODE', 'ROUTE_LIST']]
+
+# Clean up data:
+# 1). Remove stops that are Inactive (INA) Closed (CLO) or PLN
+bus_stops = bus_stops[bus_stops['STOP_STATUS'] == 'ACT']
+# 2). Remove stops that are not for Regular Service (REG)
+bus_stops = bus_stops[bus_stops['STOP_TYPE'] == 'REG']
+# 3). Filter for Seattle zipcodes
+bus_stops = bus_stops[(bus_stops["ZIPCODE"] >= 98101) & (bus_stops["ZIPCODE"] <= 98199)]
+
+# Filter for stop owners that are within Seattle city limits
+bus_stops = bus_stops[bus_stops["AUTH_NAME"].isin(['Seattle', 'Port of Seattle', 'Unknown',
+       'University of Washington', 'State Dept. of Transportation',
+       'Private Owner', 'Boeing Aircraft Corporation',
+       'South Seattle Community College', 'Federal Government'])]
+
+output_file = f'{main_path}kcm_bus_stops_cleaned.csv'
+
+bus_stops = bus_stops.reset_index(drop=True)
+bus_stops.to_csv(output_file, index=False)
