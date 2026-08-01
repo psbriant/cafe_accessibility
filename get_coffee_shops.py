77fbc1a8734720748fa624645a4fe6c9cfc5c8f5
psbriant <briantp@email.arizona.edu>
2026-07-31 17:33:30 -0700

    Added comments to make each block of code more readable

--- get_coffee_shops.py
+++ get_coffee_shops.py
@@ -8,11 +8,18 @@ import osmnx as ox
 import pandas as pd
 
 
-# Global path variables
-main_path = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
-
-
 if __name__ == "__main__":
+
+    # ------------------------------------------------------------------------
+    # ---INITIALIZE CONSTANT ARGUMENTS----------------------------------------
+    # ------------------------------------------------------------------------
+    
+    # Global path variables
+    main_path = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
+
+    # ------------------------------------------------------------------------
+    # ---PULL DATA------------------------------------------------------------
+    # ------------------------------------------------------------------------
 
     # Pull location data for Seattle coffeeshops from Open Street Maps
     place = "Seattle, United States"
@@ -20,12 +27,19 @@ if __name__ == "__main__":
     tags = {"cuisine": True}
     businesses = ox.features.features_from_place(place, tags)
 
+    # ------------------------------------------------------------------------
+    # ---PREP DATA------------------------------------------------------------
+    # ------------------------------------------------------------------------
+
     # Subset data to necessary columns only and coffee shops
     coffee_shops = businesses.copy()
     coffee_shops = coffee_shops[['name', 'cuisine', 'addr:housenumber', 'addr:street', 'addr:city', 'addr:postcode', 'geometry']]
     coffee_shops = coffee_shops[coffee_shops['cuisine'] == 'coffee_shop']
 
-    # Output data to a .csv file
+    # ------------------------------------------------------------------------
+    # ---OUTPUT DATA----------------------------------------------------------
+    # ------------------------------------------------------------------------
+
     output_file = f'{main_path}coffee_shops.csv'
 
     coffee_shops.to_csv(output_file)
