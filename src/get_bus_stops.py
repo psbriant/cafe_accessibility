9d88b2632c710bbd59984092dfebe4cb1b61e63f
psbriant <briantp@email.arizona.edu>
2026-08-13 16:46:25 -0700

    moved code into src directory to separate it from unit tests

---
+++ src/get_bus_stops.py
@@ -0,0 +1,81 @@
+"""
+Description: 
+Pull geographic metadata for King County Metro bus stops for Seattle from the 
+King County GIS Center.
+"""
+
+import logging
+import os
+
+import numpy as np
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        level=logging.INFO)
+
+    # ------------------------------------------------------------------------
+    # ---INITIALIZE CONSTANT ARGUMENTS----------------------------------------
+    # ------------------------------------------------------------------------
+    
+    main_path = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
+
+    # ------------------------------------------------------------------------
+    # ---GET DATA-------------------------------------------------------------
+    # Read in bus stop location data from King County Metro
+    # ------------------------------------------------------------------------
+    
+    logging.info("Reading in data")
+    bus_stops = pd.read_csv(f'{main_path}king_county_bus_stop_data.csv')
+
+    bus_stops = bus_stops[['STOP_ID', 
+                           'STOP_STATUS', 
+                           'XCOORD', 
+                           'YCOORD', 
+                           'STOP_TYPE', 
+                           'ON_STREET_NAME', 
+                           'HASTUS_CROSS_STREET_NAME', 
+                           'AUTH_NAME', 
+                           'ZIPCODE', 
+                           'ROUTE_LIST']]
+
+    # ------------------------------------------------------------------------
+    # ---PREP DATA------------------------------------------------------------
+    # ------------------------------------------------------------------------
+    
+    logging.info("Cleaning data")
+
+    # Clean up data:
+    # 1). Remove stops that are Inactive (INA) Closed (CLO) or PLN
+    bus_stops = bus_stops[bus_stops['STOP_STATUS'] == 'ACT']
+    # 2). Remove stops that are not for Regular Service (REG)
+    bus_stops = bus_stops[bus_stops['STOP_TYPE'] == 'REG']
+    # 3). Filter for Seattle zipcodes
+    bus_stops = bus_stops[
+        (bus_stops["ZIPCODE"] >= 98101) & (bus_stops["ZIPCODE"] <= 98199)]
+
+    logging.info("Filtering stop owners")
+
+    # Filter for stop owners that are within Seattle city limits
+    bus_stops = bus_stops[bus_stops["AUTH_NAME"].isin([
+        'Seattle', 
+        'Port of Seattle', 
+        'Unknown',
+        'University of Washington', 
+        'State Dept. of Transportation',
+        'Private Owner', 
+        'Boeing Aircraft Corporation',
+        'South Seattle Community College', 
+        'Federal Government'])]
+
+    # ------------------------------------------------------------------------
+    # ---OUTPUT DATA----------------------------------------------------------
+    # ------------------------------------------------------------------------
+    
+    logging.info("Writing data to disk")
+    output_file = f'{main_path}kcm_bus_stops_cleaned.csv'
+
+    bus_stops = bus_stops.reset_index(drop=True)
+    bus_stops.to_csv(output_file, index=False)
---
+++ src/get_coffee_shops.py
@@ -0,0 +1,61 @@
+"""
+Description: 
+Pull location data from open street maps for coffee shops in Seattle. 
+"""
+
+import logging
+
+import numpy as np
+import osmnx as ox
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    logging.basicConfig(format='%(asctime)s - %(message)s',
+                        level=logging.INFO)
+
+    # ------------------------------------------------------------------------
+    # ---INITIALIZE CONSTANT ARGUMENTS----------------------------------------
+    # ------------------------------------------------------------------------
+    
+    # Global path variables
+    main_path = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
+
+    # ------------------------------------------------------------------------
+    # ---PULL DATA------------------------------------------------------------
+    # ------------------------------------------------------------------------
+
+    # Pull location data for Seattle coffeeshops from Open Street Maps
+    logging.info("Pulling data from OSM")
+    place = "Seattle, United States"
+    aoi = ox.geocoder.geocode_to_gdf(place)
+    tags = {"cuisine": True}
+    businesses = ox.features.features_from_place(place, tags)
+
+    # ------------------------------------------------------------------------
+    # ---PREP DATA------------------------------------------------------------
+    # ------------------------------------------------------------------------
+
+    # Subset data to necessary columns only and coffee shops
+    logging.info("Cleaning up data")
+
+    coffee_shops = businesses.copy()
+    coffee_shops = coffee_shops[['name', 
+                                 'cuisine', 
+                                 'addr:housenumber', 
+                                 'addr:street', 
+                                 'addr:city', 
+                                 'addr:postcode', 
+                                 'geometry']]
+    coffee_shops = coffee_shops[coffee_shops['cuisine'] == 'coffee_shop']
+
+    # ------------------------------------------------------------------------
+    # ---OUTPUT DATA----------------------------------------------------------
+    # ------------------------------------------------------------------------
+    
+    logging.info("Outputting data") 
+    
+    output_file = f'{main_path}coffee_shops.csv'
+
+    coffee_shops.to_csv(output_file)
