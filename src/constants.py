a5cbaad5c182a9f640baa1e6f5d2feef7321e5e7
psbriant <briantp@email.arizona.edu>
2026-08-18 14:13:22 -0700

    added path to isochrome data and fixed a broken path.

--- src/constants.py
+++ src/constants.py
@@ -6,7 +6,10 @@ File for storing recurring settings.
 
 MAIN_PATH = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
 
-BUS_STOPS_CSV = f"{MAIN_PATH}kcm_bus_stops_cleaned.csv"
+BUS_STOPS_RAW_CSV = f"{MAIN_PATH}king_county_bus_stop_data.csv"
+BUS_STOPS_CLEANED_CSV = f"{MAIN_PATH}kcm_bus_stops_cleaned.csv"
 COFFEE_SHOPS_CSV = f"{MAIN_PATH}coffee_shops.csv"
 TRANSIT_ROUTES_SHP = f"{MAIN_PATH}Transit_Routes.shp"
 OUTPUT_HTML = f"{MAIN_PATH}coffee_and_bus_map.html"
+ISOCHROME_DATA = f"{MAIN_PATH}isochrome_cafe_data.json"
+
