af148a9f41e3aa9365f248f26da8855649a58f89
psbriant <briantp@email.arizona.edu>
2026-08-20 22:24:56 -0700

    Added comments and constants for the map box api token

--- src/constants.py
+++ src/constants.py
@@ -4,12 +4,20 @@ Description:
 File for storing recurring settings.
 """
 
+from dotenv import load_dotenv
+import os
+
+# Load mapbox api token
+load_dotenv() 
+TOKEN = os.environ["MAPBOX_TOKEN"]
+
+# Main file path for inputs and outputs
 MAIN_PATH = "/Users/paul_briant/Documents/coding_projects/data/cafe_accessibility/"
 
+# Paths for output files
 BUS_STOPS_RAW_CSV = f"{MAIN_PATH}king_county_bus_stop_data.csv"
 BUS_STOPS_CLEANED_CSV = f"{MAIN_PATH}kcm_bus_stops_cleaned.csv"
 COFFEE_SHOPS_CSV = f"{MAIN_PATH}coffee_shops.csv"
 TRANSIT_ROUTES_SHP = f"{MAIN_PATH}Transit_Routes.shp"
 OUTPUT_HTML = f"{MAIN_PATH}coffee_and_bus_map.html"
 ISOCHROME_DATA = f"{MAIN_PATH}isochrome_cafe_data.json"
-
